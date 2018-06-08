0b91bb360ed9aef187cfe5cae75a733d9e8a4ee6
Sean Park <sean.2015@kakaocorp.com>
2018-06-08 16:57:23 +0900

    update package

--- Package.swift
+++ Package.swift
@@ -1,9 +1,18 @@
 //
 // Created by joannis on 27-1-16.
 //
+// swift-tools-version:4.0
 
 import PackageDescription
 
 let package = Package(
-    name: "BSON"
+    name: "BSON",
+    products: [
+        .library(
+            name:"BSON",
+            targets: ["BSON"]),
+    ],
+    targets: [
+        .target(name: "BSON")
+    ]
 )
